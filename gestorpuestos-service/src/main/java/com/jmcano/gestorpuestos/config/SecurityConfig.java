b141d50eac3ea730c524e52d8286d3a5bb8fa187
Juanma Canó <juanmacanoserrano@gmail.com>
2025-05-08 16:09:38 +0200

    Security cors fix prod

--- gestorpuestos-service/src/main/java/com/jmcano/gestorpuestos/config/SecurityConfig.java
+++ gestorpuestos-service/src/main/java/com/jmcano/gestorpuestos/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://front-nexusoffice.vercel.app/"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
